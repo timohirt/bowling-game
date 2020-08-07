40e8c5eaf2b8c0d2902c0c6d58fd8ae5b8fc623f
Timo Hirt <timo.hirt@gmail.com>
2020-08-07 22:20:56 +0200

    Extend Game to handle up to ten Frames

--- src/main/java/github/com/timohirt/bowling_game/Game.java
+++ src/main/java/github/com/timohirt/bowling_game/Game.java
@@ -1,18 +1,47 @@
 package github.com.timohirt.bowling_game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     
+    private static final int MAX_FRAMES = 10;
+
     private Frame currentFrame;
+    private List<Frame> allFrames;
 
     public Game() {
         currentFrame = new Frame();
+        allFrames = new ArrayList<>();
+        allFrames.add(currentFrame);
     }
 
     public void addRoll(int pinsHit) {
-        currentFrame.addRoll(pinsHit);
+        if (currentFrame.canTakeAnotherRoll()) {
+            currentFrame.addRoll(pinsHit);
+        } else if (isLastFrame()) {
+            throw new IllegalStateException("A game only allows ten frames.");  
+        } else {
+            var newFrame = addNewFrame();
+            newFrame.addRoll(pinsHit);
+        }
+    }
+
+    private Frame addNewFrame() {
+        var newFrame = new Frame();
+        allFrames.add(newFrame);
+        currentFrame = newFrame;
+        return newFrame;
+    }
+
+    private boolean isLastFrame() {
+        return allFrames.size() >= MAX_FRAMES;
     }
 
     public Score currentScore() {
-        return currentFrame.calculateScore();
+        return allFrames
+            .stream()
+            .map(frame -> frame.calculateScore())
+            .reduce(Score.zero(), (subtotal, current) -> Score.of(subtotal.getValue() + current.getValue()));
     }
 }
--- src/test/java/github/com/timohirt/bowling_game/GameTest.java
+++ src/test/java/github/com/timohirt/bowling_game/GameTest.java
@@ -1,6 +1,9 @@
 package github.com.timohirt.bowling_game;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 import org.junit.Assert.*;
@@ -13,5 +16,35 @@ public class GameTest {
         game.addRoll(1);
 
         assertEquals(Score.of(1), game.currentScore());
+    }
+
+    @Test
+    public void testCalculateScoreOfGameWithTwoFrames() {
+        var game = new Game();
+        addRolls(game, 3, 1);
+
+        assertEquals(Score.of(3), game.currentScore());
+    }
+
+    @Test
+    public void testCalculateScoreOfGameWithTenFrames() {
+        var game = new Game();
+        addRolls(game, 20, 1);
+        
+        assertEquals(Score.of(20), game.currentScore());
+    }
+
+    @Test
+    public void testThrowExceptionIfMoreThanTenFramesAdded() {
+        var game = new Game();
+        addRolls(game, 20, 1);
+        
+        assertThrows(IllegalStateException.class, () -> game.addRoll(1));
+    }
+
+    private void addRolls(Game game, int numberOfRolls, int pinsHit) {
+        IntStream
+            .range(0, numberOfRolls)
+            .forEach(irrelevant -> game.addRoll(pinsHit));
     }
 }
